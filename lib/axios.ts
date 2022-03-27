08ed6aee3127c53c95ec5764eadbd564ae214c02
peterkwesiansah <ansahpeter123@yahoo.com>
2022-03-27 17:20:34 +0000

    fixed hostname

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,64 +1,64 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios, { AxiosRequestConfig } from "axios";
 
 interface axiosProps {
-	path: string;
-	data?: {};
-	contentType?: string;
-	params?: {};
-	headers?: {};
-	method:
-		| 'get'
-		| 'GET'
-		| 'delete'
-		| 'DELETE'
-		| 'head'
-		| 'HEAD'
-		| 'options'
-		| 'OPTIONS'
-		| 'post'
-		| 'POST'
-		| 'put'
-		| 'PUT'
-		| 'patch'
-		| 'PATCH'
-		| 'link'
-		| 'LINK'
-		| 'unlink'
-		| 'UNLINK'
-		| undefined;
-	noHeaders?: boolean;
+  path: string;
+  data?: {};
+  contentType?: string;
+  params?: {};
+  headers?: {};
+  method:
+    | "get"
+    | "GET"
+    | "delete"
+    | "DELETE"
+    | "head"
+    | "HEAD"
+    | "options"
+    | "OPTIONS"
+    | "post"
+    | "POST"
+    | "put"
+    | "PUT"
+    | "patch"
+    | "PATCH"
+    | "link"
+    | "LINK"
+    | "unlink"
+    | "UNLINK"
+    | undefined;
+  noHeaders?: boolean;
 }
 
 interface Config extends AxiosRequestConfig {
-	contentType?: string;
-	data?: {};
-	json: boolean;
+  contentType?: string;
+  data?: {};
+  json: boolean;
 }
 
 export type headerType = {
-	Authorization: string;
-	'Content-Type': string;
+  Authorization: string;
+  "Content-Type": string;
 };
 
 export const fetchAsync = async ({
-	path,
-	noHeaders,
-	data,
-	...rest
+  path,
+  noHeaders,
+  data,
+  ...rest
 }: axiosProps) => {
-	const hostname =process.env.NEXT_PUBLIC_API_URL
-	const url = `${hostname}${path}`;
-	const axiosData: Config = {
-		...rest,
-		data,
-		json: true,
-		url,
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		xsrfCookieName: 'XSRF-TOKEN',
-		xsrfHeaderName: 'X-XSRF-TOKEN',
-	};
-	const payload = await axios(axiosData);
-	return payload.data;
+  const hostname = window.location.origin + "/api";
+  const url = `${hostname}${path}`;
+  const axiosData: Config = {
+    ...rest,
+    data,
+    json: true,
+    url,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    xsrfCookieName: "XSRF-TOKEN",
+    xsrfHeaderName: "X-XSRF-TOKEN",
+  };
+  const payload = await axios(axiosData);
+  return payload.data;
 };
